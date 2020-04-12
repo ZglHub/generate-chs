67aec580c8cda9a1f1a01a63ffa430d2cbd1397a
GuoliangZhang <819894769@qq.com>
2020-04-12 10:48:45 +0800

    初始化

---
+++ src/main/java/com/zgl/generatechs/GenerateChsApplication.java
@@ -0,0 +1,13 @@
+package com.zgl.generatechs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerateChsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenerateChsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zgl/generatechs/GenerateChsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zgl.generatechs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerateChsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
